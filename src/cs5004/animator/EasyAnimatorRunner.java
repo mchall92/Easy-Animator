3a4973fcfb0bf15f0a47c956037a2d09ba4c7cec
Martin C <martinchien92@gmail.com>
2021-04-26 00:14:51 +0800

    fix runner style

--- src/cs5004/animator/EasyAnimatorRunner.java
+++ src/cs5004/animator/EasyAnimatorRunner.java
@@ -21,7 +21,19 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 
+/**
+ * This is the starter class for our EasyAnimator.
+ */
+
 public class EasyAnimatorRunner {
+
+  /**
+   * This runs our EasyAnimator by instantiating model and view and pass them
+   * to our controller.
+   * @param args args is the argument that decides how our animator view will run.
+   *
+   * @throws IOException if there is an I/O error when reading input file.
+   */
   public static void main(String[] args) throws IOException {
     System.out.println(Arrays.toString(args));
     EasyAnimatorSetter easyAnimator = new EasyAnimatorSetter(args);
@@ -61,6 +73,10 @@ public class EasyAnimatorRunner {
           IController controller = new PlaybackController(emptyWindow, view, argsMap);
           controller.go();
         }
+      }
+      default: {
+        System.out.println("Should not reach here, error with argsParse and "
+            + "EasyAnimatorSetter.");
       }
     }
   }
